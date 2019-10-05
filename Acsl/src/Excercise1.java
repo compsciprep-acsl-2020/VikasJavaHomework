930021ad61ea50d57ed5c83fb91ea75701e469fc
V_Nanduri <nandurivikas@gmail.com>
2019-10-05 11:17:48 -0400

    push commit

---
+++ Acsl/src/Excercise1.java
@@ -0,0 +1,13 @@
+public class Excercise1 {
+    public static int sumMupltiple(int num, int limit) {
+        int Mulitples = (limit - 1) / num;
+        return num * (Mulitples * (Mulitples + 1) / 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                sumMupltiple(3, 1000) + sumMupltiple(5, 1000) - sumMupltiple(15, 1000));
+
+    }
+}
+}
